e715b03dc8fa6f217761ff7bb147188ca5e35802
[GDGOMES] Gustavo da Silva Gomes <gustavo.sgomes@embraer.com.br>
2025-11-15 21:26:11 -0300

    feat: add typescript support with prettier formatting

--- lua/custom/plugins/lsp/lsp.lua
+++ lua/custom/plugins/lsp/lsp.lua
@@ -11,23 +11,6 @@ return {
     local capabilities = require('blink.cmp').get_lsp_capabilities()
 
     local servers = {
-      tsserver = {
-        -- Configurações TypeScript
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-          end
-
-          map('grn', vim.lsp.buf.rename, 'LSP Rename')
-          map('grr', vim.lsp.buf.references, 'LSP References')
-          map('grd', vim.lsp.buf.definition, 'LSP Definition')
-        end,
-        settings = {
-          -- Habilita sugestões automáticas de imports
-          completions = { completeFunctionCalls = true },
-        },
-      },
       lua_ls = {
         settings = {
           Lua = {
@@ -38,7 +21,9 @@ return {
     }
 
     require('mason-tool-installer').setup {
-      ensure_installed = vim.tbl_keys(servers),
+      ensure_installed = vim.list_extend(vim.tbl_keys(servers), {
+        'prettier', -- Formatador para JavaScript/TypeScript
+      }),
     }
 
     require('mason-lspconfig').setup {
--- lua/custom/plugins/lsp/typescript-tools.lua
+++ lua/custom/plugins/lsp/typescript-tools.lua
@@ -1,5 +1,26 @@
 return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  opts = {},
+  opts = {
+    settings = {
+      -- Desabilita diagnósticos do tsserver (usa eslint quando disponível)
+      diagnosticMode = 'off',
+      -- Habilita sugestões automáticas de imports
+      completions = {
+        completeFunctionCalls = true,
+      },
+      -- Habilita refatorações do TypeScript
+      enableLineBreakOnTypedRangeEnd = true,
+      inlayHints = {
+        enabled = true,
+        includeInlayEnumMemberValueHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayParameterNameHints = 'all',
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayVariableTypeHints = true,
+      },
+    },
+  },
 }
